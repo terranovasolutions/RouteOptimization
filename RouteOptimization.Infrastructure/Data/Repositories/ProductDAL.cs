f08533ab6a4631b175551f50542bab0c07f3f06f
Sahil Sharma <sahil0601@gmail.com>
2015-02-10 09:17:53 -0500

    DataviewModel class

--- RouteOptimization.Infrastructure/Data/Repositories/ProductDAL.cs
+++ RouteOptimization.Infrastructure/Data/Repositories/ProductDAL.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RouteOptimization;
 
 namespace RouteOptimization.Infrastructure
 {
     public class ProductDAL
     {
+        RouteOptimizationDBEntities db = new RouteOptimizationDBEntities();
         public void GetAllProducts()
         {
 
         }
+        public IEnumerable<Product> ListProducts()
+        {
+            return db.Products.ToList();
+        }
+
+        public Product GetProductById(int productId)
+        {
+            var result = (from product in db.Products
+                          where product.ProductID == productId
+                          select product).FirstOrDefault<Product>();
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string AddorUpdate(Product product)
+        {
+            if (product.ProductID > 0)
+            {
+                Product pid = this.GetProductById(product.ProductID);
+                db.Entry(pid).CurrentValues.SetValues(product);
+            }
+            else
+            {
+                db.Products.Add(product);
+            }
+            db.SaveChanges();
+            return "";
+        }
+
+        public void Delete(int id)
+          {
+              var temp = (from x in db.Products
+                          where x.ProductID == id
+                          select x).FirstOrDefault();
+              db.Products.Remove(temp);
+              db.SaveChanges();
+          }
     }
 
 }
---
+++ RouteOptimization.Infrastructure/Data/ViewModels/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteOptimization.Infrastructure
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+
+        public List<Product> AllProducts { get; set; }
+    }
+}
