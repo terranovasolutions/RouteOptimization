98cd4a51bd3c2fe24d8c7c40c8682fadfe4d4d42
pallavibhatnagar <pallavibhatnagar05@gmail.com>
2015-02-10 18:58:01 -0500

    Adding Field Employee Module

---
+++ RouteOptimizationUI/Models/FieldEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteOptimizationUI.Models
+{
+    public class FieldEmployee
+    {
+        public int employeeId { get; set; }
+
+        public string employeeName { get; set; }
+
+        public string employeeAddress { get; set; }
+
+
+    }
+}
